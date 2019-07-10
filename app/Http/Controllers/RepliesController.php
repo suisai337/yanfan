a712ca11601d626e0c44dc6919d82e49765d52ce
Yanfan <330483691@qq.com>
2019-07-10 22:24:52 +0800

    消息通知有新回复

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reply;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,13 +15,14 @@ class RepliesController extends Controller
 
 	public function store(ReplyRequest $request, Reply $reply)
 	{
-        $reply->content = $request->content;
+        $reply->content = $request->contents;
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->topic_id;
         $reply->save();
 
         return redirect()->to($reply->topic->link())->with('success', '评论创建成功！');
 	}
+
 
 	public function destroy(Reply $reply)
 	{
